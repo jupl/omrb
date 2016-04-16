ac46f941e8a95f2218010b9ec285b0c4f7d5f335
jupl <juan.placencia.512@gmail.com>
2016-04-15 21:52:40 -0500

    Add some styling to the component

--- src/app/components.jsx
+++ src/app/components.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import prefix from 'react-prefixer'
+
+const STYLE = prefix({
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  background: 'linear-gradient(white, gainsboro)',
+})
 
 export default function App() {
-  return (
-    <div>Welcome</div>
-  )
+  return <div style={STYLE}>Welcome</div>
 }
