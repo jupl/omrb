b69732a365be054c53066345fc3f47a907ee07f4
jupl <juan.placencia.512@gmail.com>
2016-06-04 11:29:53 -0500

    Document, reorganize, and simplify render code

--- src/index.js
+++ src/index.js
@@ -14,30 +14,24 @@ render()
 
 /**
  * Render application to the container. If we are not in production and an
- * error is encountered the error is rendered to the screen instead. This may
- * be called multiple times to rerender when a hot reload occurs.
+ * error is encountered the error is rendered to the screen. This may be called
+ * multiple times to rerender when a hot reload occurs.
  * @return {undefined} Nothing
  */
 function render() {
-  if(process.env.NODE_ENV === 'production') {
-    renderRoot()
+  try {
+    // For hot loading support require is used instead of import so that if
+    // this function is called again, then the updated code is loaded
+    const Root = require('./app/components/root').default
+    renderToDOM(<Root />, container)
   }
-  else {
-    try {
-      renderRoot()
-    }
-    catch(e) {
+  catch(e) {
+    if(process.env.NODE_ENV !== 'production') {
+      // Production builds will not have this code included, which is why
+      // require is used instead of import to exclude redbox code
       const RedBox = require('redbox-react')
       renderToDOM(<RedBox error={e} />, container)
     }
+    throw e
   }
-}
-
-/**
- * Render root component to the container
- * @return {undefined} Nothing
- */
-function renderRoot() {
-  const Root = require('./app/components/root').default
-  renderToDOM(<Root />, container)
 }
