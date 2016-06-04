a30709a39510eb9c659912c5ef93890beba3e4e1
jupl <juan.placencia.512@gmail.com>
2016-06-03 22:39:16 -0500

    Better document and structure entry point

--- src/index.js
+++ src/index.js
@@ -1,32 +1,44 @@
 import React from 'react'
 import 'normalize.css/normalize.css'
-import {render} from 'react-dom'
+import {render as renderToDOM} from 'react-dom'
 
+// Reference app container and unhide it before rendering to
 const {container} = global
-
 container.style.display = null
 
-if(module.hot) {
-  module.hot.accept('./app/components/root', () => {
-    setTimeout(renderRootOrError)
-  })
-  renderRootOrError()
-}
-else {
-  renderRoot()
-}
+// If in production, render root component. Otherwise render component or an
+// error message if failed to render.
+const production = process.env.NODE_ENV === 'production'
+const render = production ? renderRoot : renderRootOrError
 
+// Render root, and register to rerender if hot loading is available
+if(module.hot) {
+  module.hot.accept('./app/components/root', () => setTimeout(render))
+}
+render()
+
+/**
+ * Render root component to the container. This may be called multiple times
+ * to rerender when a hot reload occurs.
+ * @return {undefined} Nothing
+ */
 function renderRoot() {
   const Root = require('./app/components/root').default
-  render(<Root />, container)
+  renderToDOM(<Root />, container)
 }
 
+/**
+ * Try to render root component to the container. If an error is encountered
+ * the error is displayed to the screen instead. This may be called multiple
+ * times to rerender when a hot reload occurs.
+ * @return {undefined} Nothing
+ */
 function renderRootOrError() {
   try {
     renderRoot()
   }
   catch(e) {
     const RedBox = require('redbox-react')
-    render(<RedBox error={e} />, container)
+    renderToDOM(<RedBox error={e} />, container)
   }
 }
