3d750c27c9239d420341ee6768666204654cdd76
jupl <juan.placencia.512@gmail.com>
2016-06-05 13:05:40 -0500

    Make doc test ID more specific

--- src/app/components/root/template.test.js
+++ src/app/components/root/template.test.js
@@ -3,7 +3,7 @@ import test from 'ava'
 import {render} from 'enzyme'
 import Root from './template'
 
-/** @test {Root} */
+/** @test {app.components.Root} */
 test('<Root>', t => {
   const wrapper = render(<Root />)
   t.is(wrapper.text(), 'Welcome')
