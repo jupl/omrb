a732862b5d3c8b122c4163c202c3b929a87fc074
jupl <juan.placencia.512@gmail.com>
2016-06-05 12:44:21 -0500

    Move babel-register from setup to ava require

--- setup-tests.js
+++ setup-tests.js
@@ -1,6 +1,3 @@
-// Register Babel since our tests require transpiling
-require('babel-register')
-
 // Simulate a browser environment in Node with jsdom
 const {jsdom} = require('jsdom')
 const document = global.document = jsdom('')
