db2e4ce50da5cbbe24a88d796a7f192480d637c8
jupl <juan.placencia.512@gmail.com>
2016-06-05 19:14:59 -0500

    Fix hot reloading of components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+import React, {Component} from 'react'
 import {AppRegistry} from 'react-native'
 import Root from './app/components/root'
 
 // Supported platforms for React Native
 const PLATFORMS = ['android', 'ios']
+
+// Flag to keep track if this is a first load for hot reloading purposes
+let firstLoad = true
+
+/** Component that contains root component as a class for hot reloading */
+class Application extends Component {
+  render() {
+    return <Root />
+  }
+}
 
 /**
  * Entry point for application
@@ -15,6 +26,13 @@ export default function start(platform) {
     throw new Error('Platform is not supported')
   }
 
+  // Run any required first-time code
+  if(firstLoad) {
+    firstLoad = false
+
+    // Add first-time code to run here
+  }
+
   // Tell React Native to use our root component to render application
-  AppRegistry.registerComponent('App', () => Root)
+  AppRegistry.registerComponent('Application', () => Application)
 }
