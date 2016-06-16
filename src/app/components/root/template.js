dfc28228d8aa6ea370aec5d3262cd88a9ad2ecf9
jupl <juan.placencia.512@gmail.com>
2016-06-15 21:23:33 -0500

    Use classes for components for HMR friendliness

--- src/app/components/root/template.js
+++ src/app/components/root/template.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {StyleSheet, Text, View} from 'react-native'
 import * as platform from '../../../lib/platform'
 import * as rawStyles from './styles'
@@ -6,15 +6,13 @@ import * as rawStyles from './styles'
 // Create React Native stylesheet from style data
 const styles = StyleSheet.create(rawStyles)
 
-/**
- * Render root component representing the entire application
- * @param {Object} props - Component properties passed on
- * @return {ReactElement} Root component
- */
-export default function Root() {
-  return (
-    <View style={styles.CONTAINER}>
-      <Text style={styles.TEXT}>Welcome to {platform.NAME}</Text>
-    </View>
-  )
+/** Root component that represents the entire application */
+export default class Root extends Component {
+  render() {
+    return (
+      <View style={styles.CONTAINER}>
+        <Text style={styles.TEXT}>Welcome to {platform.NAME}</Text>
+      </View>
+    )
+  }
 }
