7d3a4cc8720d0b9fbe236027c2cd50ac765c0025
jupl <juan.placencia.512@gmail.com>
2016-10-08 16:38:07 -0500

    Fix test and use stateless component

--- src/app/components/root/template.js
+++ src/app/components/root/template.js
@@ -1,17 +1,18 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {Platform, StyleSheet, Text, View} from 'react-native'
 import * as rawStyles from './styles'
 
 // Create React Native stylesheet from style data
 const {CONTAINER_STYLE, TEXT_STYLE} = StyleSheet.create(rawStyles)
 
-/** Root component that represents the entire application */
-export default class Root extends Component {
-  render() {
-    return (
-      <View style={CONTAINER_STYLE}>
-        <Text style={TEXT_STYLE}>Welcome to {Platform.OS}</Text>
-      </View>
-    )
-  }
+/**
+ * Render root component representing the entire application
+ * @return {ReactElement} Root component
+ */
+export default function Root() {
+  return (
+    <View style={CONTAINER_STYLE}>
+      <Text style={TEXT_STYLE}>Welcome to {Platform.OS}</Text>
+    </View>
+  )
 }
